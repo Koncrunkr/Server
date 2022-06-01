1782d55e83393b14c7b0b12fb9f7708ffd4bf318
MediaNik5 <stafilopok@mail.ru>
2022-06-01 12:39:29 +0300

    Fix nullpointer exception

--- src/main/java/ru/comgrid/server/util/ColorConverter.java
+++ src/main/java/ru/comgrid/server/util/ColorConverter.java
@@ -7,11 +7,11 @@ import javax.persistence.Converter;
 public class ColorConverter implements AttributeConverter<Color, String>{
     @Override
     public String convertToDatabaseColumn(Color attribute){
-        return attribute.toString();
+        return attribute == null ? null : attribute.toString();
     }
 
     @Override
     public Color convertToEntityAttribute(String dbData){
-        return Color.ofString(dbData);
+        return dbData == null ? null : Color.ofString(dbData);
     }
 }
