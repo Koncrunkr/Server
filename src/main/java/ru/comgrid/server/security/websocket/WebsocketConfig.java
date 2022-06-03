da712f5ae89aa06e4a1f82c82b595bcab1b7aa6d
MediaNik5 <stafilopok@mail.ru>
2022-06-03 11:53:43 +0300

    Add test site to allowed redirect uris

--- src/main/java/ru/comgrid/server/security/websocket/WebsocketConfig.java
+++ src/main/java/ru/comgrid/server/security/websocket/WebsocketConfig.java
@@ -63,8 +63,8 @@ public class WebsocketConfig extends AbstractSecurityWebSocketMessageBrokerConfi
 
     @Override
     public void registerStompEndpoints(@NonNull StompEndpointRegistry registry){
-        registry.addEndpoint("/websocket").setAllowedOrigins("https://comgrid.ru", "http://localhost:3000", "http://localhost:8080").withSockJS();
-        registry.addEndpoint("/websocket").setAllowedOrigins("https://comgrid.ru", "http://localhost:3000", "http://localhost:8080");
+        registry.addEndpoint("/websocket").setAllowedOrigins("https://comgrid.ru", "https://comgrid.ru:444", "http://localhost:3000", "http://localhost:8080").withSockJS();
+        registry.addEndpoint("/websocket").setAllowedOrigins("https://comgrid.ru", "https://comgrid.ru:444", "http://localhost:3000", "http://localhost:8080");
     }
 
     @Override
